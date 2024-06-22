0c6fefb113cec1c72ff77230f2a136e46c378aa2
Gleb Svirin <glebcore@gmail.com>
2024-06-22 14:26:23 +0300

    add calculator

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
+import { sanityIntegration } from '@sanity/astro';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 
     site: 'https://test-gleb.github.io',
+
+    integrations: [
+        sanityIntegration({
+            projectId: '5lzavdfu',
+            dataset: 'production',
+            apiVersion: "2024-03-11",
+            useCdn: false
+        }),
+    ],
    
 
 });
