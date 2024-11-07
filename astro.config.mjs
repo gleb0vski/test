18565b2f756b784b1a1c80825631f0625a45aef1
Gleb <54006945+gleb0vski@users.noreply.github.com>
2024-11-07 11:11:35 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ego-trade.ru/',
+  site: '',
   integrations: [sanityIntegration({
     projectId: '5lzavdfu',
     dataset: 'production',
